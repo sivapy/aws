f5f78fc0fc00ddf2d078c6431569154c433c1fcb
sivapy <36306257+sivapy@users.noreply.github.com>
2023-03-31 16:02:40 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "proj1" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Project1 VPC"
+  }
+}
+
+resource "aws_vpc" "proj3" {
+  cidr_block = "10.10.0.0/16"
+
+  tags = {
+    Name = "Project2 VPC"
+  }
+}
