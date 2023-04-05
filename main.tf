79de850b8d7cf5dfd27e938ff453be4f56bbe3c5
sivapy <36306257+sivapy@users.noreply.github.com>
2023-04-06 00:48:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,44 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
-  region = "ap-south-1"
+    region = "us-east-1"  
 }
 
-resource "aws_vpc" "proj-1" {
-  cidr_block = "10.10.0.0/16"
-
+resource "aws_instance" "foo" {
+  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  instance_type = "t2.micro"
   tags = {
-    Name = "Project1 VPC"
-  }
-}
-
-resource "aws_vpc" "proj-2" {
-  cidr_block = "10.20.0.0/16"
-
-  tags = {
-    Name = "Project2 VPC"
-  }
-}
-resource "aws_vpc" "proj-3" {
-  cidr_block = "10.30.0.0/16"
-
-  tags = {
-    Name = "Project3 VPC"
-  }
-}
-resource "aws_vpc" "proj-4" {
-  cidr_block = "10.40.0.0/16"
-
-  tags = {
-    Name = "Project4 VPC"
+      Name = "TF-Instance"
   }
 }
