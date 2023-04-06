79b729881b9f7e0d81a0e47e5427b959e66259e6
sivapy <36306257+sivapy@users.noreply.github.com>
2023-04-06 19:33:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,19 +13,4 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_vpc" "proj-1" {
-  cidr_block = "10.10.0.0/16"
-
-  tags = {
-    Name = "Project1 VPC"
-  }
-}
-
-resource "aws_vpc" "proj-2" {
-  cidr_block = "10.20.0.0/16"
-
-  tags = {
-    Name = "Project2 VPC"
-  }
-}
 
