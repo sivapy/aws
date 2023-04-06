ac21212d514a86c38431689221783e459cf76214
sivapy <imsiva89@gmail.com>
2023-04-06 21:44:43 +0530

    cidr change

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "proj--1" {
-  cidr_block = "10.10.0.0/16"
+  cidr_block = "10.100.0.0/16"
 
   tags = {
     Name = "Project1 VPC"
@@ -22,7 +22,7 @@ resource "aws_vpc" "proj--1" {
 }
 
 resource "aws_vpc" "proj3" {
-  cidr_block = "10.10.0.0/16"
+  cidr_block = "10.200.0.0/16"
 
   tags = {
     Name = "Project2 VPC"
